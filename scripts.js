b11489d480767987927571670c7c711f508435f7
OfentseXI <dofentse69@gmail.com>
2024-05-24 10:55:14 +0200

    Defining of actions

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
 const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
 const RESET = "RESET";
+
+const increment = () => ({ type: INCREMENT });
+const decrement = () => ({ type: DECREMENT });
+const reset = () => ({ type: RESET });
