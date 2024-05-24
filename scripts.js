4fa9e0e9536507eaf1a5ec34794aafc06fbe2e16
OfentseXI <dofentse69@gmail.com>
2024-05-24 10:58:24 +0200

    Store class

--- scripts.js
+++ scripts.js
@@ -21,3 +21,26 @@ const tallyReducer = (state = initialState, action) => {
     }
   };
 
+  class Store {
+    constructor(reducer, initialState) {
+      this.reducer = reducer;
+      this.state = initialState;
+      this.listeners = [];
+    }
+  
+    getState() {
+      return this.state;
+    }
+  
+    dispatch(action) {
+      this.state = this.reducer(this.state, action);
+      this.listeners.forEach((listener) => listener());
+    }
+  
+    subscribe(listener) {
+      this.listeners.push(listener);
+      return () => {
+        this.listeners = this.listeners.filter((l) => l !== listener);
+      };
+    }
+  }
