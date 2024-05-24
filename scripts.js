dbdf0bec10ad33821501c42bed3c82a035ae4b3f
OfentseXI <dofentse69@gmail.com>
2024-05-24 10:56:52 +0200

    Defining of reducer

--- scripts.js
+++ scripts.js
@@ -7,3 +7,16 @@ const decrement = () => ({ type: DECREMENT });
 const reset = () => ({ type: RESET });
 
 const initialState = { count: 0 };
+
+const tallyReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case INCREMENT:
+        return { count: state.count + 1 };
+      case DECREMENT:
+        return { count: state.count - 1 };
+      case RESET:
+        return { count: 0 };
+      default:
+        return state;
+    }
+  };
